14ff9639b2a0042ab78fce708bb445acc80c250f
Itay Margolin <itaym@dsiaspl01.local.biu.ac.il>
2022-09-17 20:31:49 +0300

    added documentation for loss utils

--- ordinaloss/utils/loss_utils.py
+++ ordinaloss/utils/loss_utils.py
@@ -20,23 +20,27 @@ class CSCELoss(nn.Module):
         self.smoothing = smoothing
         
     def forward(self,y_pred, y_true):
+        '''
+        y_pred is shape of (N, C) - Normalized
+        y_true is shape of (N) Integers.
+        '''
         
         #y_pred is going under smoothing,
         y_pred = y_pred * (1-self.smoothing) + (1/self.n_classes) * self.smoothing
         
         #y_pred shape is [batch_size, num_classes]
         #y_true indexes of the [batch_size, ] 
-        weights = self.cb_matrix[y_true]
-        
+        weights = self.cb_matrix[y_true]        
         y_true = F.one_hot(y_true, num_classes = self.n_classes)
         
         loss = (y_true * torch.log(y_pred)) + ((1-y_true) * torch.log(1-y_pred))
         
         return -1* (loss * weights).sum(axis=1).mean() #Returns as avg across all samples
+
+
     
     def to(self, device):
         self.cb_matrix = self.cb_matrix.to(device)
-
 
 class SinimLoss(nn.Module):
     def __init__(self, ordinal_matrix):
@@ -52,6 +56,10 @@ class SinimLoss(nn.Module):
         self.ordinal_matrix = self.ordinal_matrix.cuda()
 
     def forward(self, y_pred, y_true):
+        '''
+        y_pred is shape of (N, C) - Normalized
+        y_true is shape of (N) Integers.
+        '''
         weights = self.ordinal_matrix[y_true]
         y_true = F.one_hot(y_true, num_classes = self.n_classes)
         return torch.sum((y_pred * weights) ** 2) / y_pred.shape[0]
@@ -75,7 +83,9 @@ class GirlsLoss(nn.Module):
     
     def forward(self, y_pred, y_true):
         '''
-        outputs are normalized
+
+        y_pred is shape of (N, C) - Normalized
+        y_true is shape of (N) Integers.
         '''
 
         weights = self.ordinal_matrix[y_true]
@@ -84,15 +94,11 @@ class GirlsLoss(nn.Module):
         loss = -1 * torch.sum((
                 y_true * torch.log(y_pred) + (1 - y_true) *
                 torch.log(1 - y_pred))* weights) / y_pred.shape[0]
-
-
         return loss
 
     def to(self, device):
         self.ordinal_matrix = self.ordinal_matrix.to(device)
         return self
-
-
 
 class SinimLossOld(nn.Module):
     def __init__(self):
